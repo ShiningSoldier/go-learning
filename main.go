f336977d9fcf4e8496d4a713848fa06b26a6f16e
dmitry_buimistrov <dmitry.buimistrov@instructure.com>
2021-04-16 11:20:42 +0300

    Implement task 4 - use the correct flow

--- main.go
+++ main.go
@@ -3,16 +3,49 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 )
 
 func main() {
-	var s = "J'n mfbsojoh hp qsphsbnnjoh mbohvbhf"
-	fmt.Print(decrypt(s, 1))
+	var encryptedText = "Olssv, tf mypluk. Ovd hyl fvb kvpun?"
+	var decryptedText = []string{"Doing", "how", "you", "friend", "my"}
+	var decryptedString = strings.Join(decryptedText, " ")
+	decryptedString = strings.ToLower(decryptedString)
+	decryptedText = strings.Split(decryptedString, " ")
+
+	fmt.Printf(decryptString(encryptedText, decryptedText))
+}
+
+func decryptString(encryptedText string, decryptedText []string) string {
+	s := strings.ToLower(encryptedText)
+	sort.Strings(decryptedText)
+
+	var step byte = 0
+	result := decrypt(s, step)
+
+	for compareTexts(result, decryptedText) != true {
+		step++
+		result = decrypt(s, step)
+	}
+
+	return fmt.Sprintf("Full text: %s, key: %d", result, step)
+}
+
+func compareTexts(result string, decryptedText []string) bool {
+	splitResult := strings.Split(result, " ")
+	firstWord := decryptedText[0]
+	secondWord := decryptedText[1]
+	thirdWord := decryptedText[2]
+
+	if contains(splitResult, firstWord) == false && contains(splitResult, secondWord) == false && contains(splitResult, thirdWord) == false {
+		return false
+	}
+
+	return true
 }
 
 func decrypt(s string, step byte) string {
-	s = strings.ToLower(s)
 	strToBytes := []byte(s)
 
 	var result = make([]byte, len(strToBytes), len(strToBytes))
@@ -31,4 +64,14 @@ func decrypt(s string, step byte) string {
 	}
 
 	return string(result)
+}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
 }
